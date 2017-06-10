c35b7aafde850a43db93920e8a2c1d423520a3d7
Jeff Reback <jeff.reback@twosigma.com>
2017-06-10 11:08:28 -0400

    testing for master

--- pandas_compat/__init__.py
+++ pandas_compat/__init__.py
@@ -1,5 +1,42 @@
+# -*- coding: utf-8 -*-
 
 from ._version import get_versions
+from distutils.version import LooseVersion
+import pandas as pd
+
+_pdv = LooseVersion(pd.__version__)
+_pd_version_under_017 = _pdv < '0.17.0'
+_pd_version_under_018 = _pdv < '0.18.0'
+_pd_version_under_019 = _pdv < '0.19.0'
+_pd_version_under_020 = _pdv < '0.20.0'
+_pd_version_under_021 = _pdv < '0.21.0'
+_pd_version_under_100 = _pdv < '1.0.0'
+_pd_version_is_019 = _pdv.version[1] == 19 and len(_pdv.version) == 3
+
+
+# api imports
+
+# >= 0.20.0
+if not _pd_version_under_020:
+    from pandas.api.types import *  # noqa
+
+# >= 0.19.0
+elif not _pd_version_under_019:
+    from pandas.api.types import *  # noqa
+    from pandas.types.dtypes import DatetimeTZDtype, CategoricalDtype  # noqa
+
+# >= 0.18.0
+elif not _pd_version_under_018:
+    from pandas.core.common import DatetimeTZDtype, CategoricalDtype  # noqa
+
+# >= 0.17.0
+elif not _pd_version_under_017:
+    from pandas.core.common import DatetimeTZDtype, CategoricalDtype  # noqa
+
+else:
+    raise ValueError("pandas < 0.17.0 is not supported for pandas-compat")
+
+
 versions = get_versions()
 __version__ = versions.get('closest-tag', versions['version'])
 __git_revision__ = versions['full-revisionid']
--- pandas_compat/tests/test_compat.py
+++ pandas_compat/tests/test_compat.py
@@ -1,5 +1,40 @@
+# -*- coding: utf-8 -*-
+
 import pytest
+import pandas_compat as pdc
 
 
-def test_basic():
-    pass
+@pytest.mark.parametrize("func", [
+    'is_bool', 'is_bool_dtype',
+    'is_categorical', 'is_categorical_dtype', 'is_complex',
+    'is_complex_dtype', 'is_datetime64_any_dtype',
+    'is_datetime64_dtype', 'is_datetime64_ns_dtype',
+    'is_datetime64tz_dtype', 'is_datetimetz', 'is_dtype_equal',
+    'is_extension_type', 'is_float', 'is_float_dtype',
+    'is_int64_dtype', 'is_integer',
+    'is_integer_dtype', 'is_number', 'is_numeric_dtype',
+    'is_object_dtype', 'is_scalar', 'is_sparse',
+    'is_string_dtype', 'is_signed_integer_dtype',
+    'is_timedelta64_dtype', 'is_timedelta64_ns_dtype',
+    'is_unsigned_integer_dtype', 'is_period',
+    'is_period_dtype', 'is_interval', 'is_interval_dtype',
+    'is_re', 'is_re_compilable',
+    'is_dict_like', 'is_iterator', 'is_file_like',
+    'is_list_like', 'is_hashable',
+    'is_named_tuple',
+    'pandas_dtype', 'union_categoricals', 'infer_dtype'])
+def test_introspection(func):
+    result = getattr(pdc, func)
+    assert result is not None
+
+
+@pytest.mark.parametrize("dtype", [
+    'CategoricalDtype',
+    'DatetimeTZDtype',
+    pytest.mark.skipif(
+        pdc._pd_version_under_019, reason='too old version')('PeriodDtype'),
+    pytest.mark.skipif(
+        pdc._pd_version_under_019, reason='too old version')('IntervalDtype')])
+def test_dtypes(dtype):
+    result = getattr(pdc, dtype)
+    assert result is not None
